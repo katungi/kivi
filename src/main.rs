602ae86beacb854f491c74d5a508d0f735346921
katungi <dankatdennis@gmail.com>
2021-04-04 16:01:51 +0300

    getting user input for key and value

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    // get all arguments sent by user
+  let mut arguments = std::env::args().skip(1);
+
+  // get the next value in the arguments --> we get back an option string because the option might be there or not String or null
+  // sigh, hakuna null kwa Rust
+
+  let key = arguments.next().unwrap();
+  let value = arguments.next().unwrap();
+  // println!("The Key is '{}' and the value is {}", key, value);
+
+
 }
