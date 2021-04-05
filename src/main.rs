0c772308307c848d7b09e4a0047083a0977959eb
katungi <dankatdennis@gmail.com>
2021-04-05 05:23:18 +0300

    wrote to a db file 🦞

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,12 @@ fn main() {
   // get the next value in the arguments --> we get back an option string because the option might be there or not String or null
   // sigh, hakuna null kwa Rust
 
-  let key = arguments.next().unwrap();
+  let key = arguments.next().expect("Key was not found");
   let value = arguments.next().unwrap();
   // println!("The Key is '{}' and the value is {}", key, value);
-
   // we need a place to save the data. For now we will use a file.
 
+  let contents = format!("{}\t{}\n",key, value);
+  fs::write("kv.db", contents);
 
 }
