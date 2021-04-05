639a8ade79be897e47f40df4a671e732e78b53ad
katungi <dankatdennis@gmail.com>
2021-04-05 05:32:06 +0300

    error handling 🦞

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,19 @@ use std::fs;
 
 fn main() {
     // get all arguments sent by user
-  let mut arguments = std::env::args().skip(1);
+    let mut arguments = std::env::args().skip(1);
 
-  // get the next value in the arguments --> we get back an option string because the option might be there or not String or null
-  // sigh, hakuna null kwa Rust
+    // get the next value in the arguments --> we get back an option string because the option might be there or not String or null
+    // sigh, hakuna null kwa Rust
 
-  let key = arguments.next().expect("Key was not found");
-  let value = arguments.next().unwrap();
-  // println!("The Key is '{}' and the value is {}", key, value);
-  // we need a place to save the data. For now we will use a file.
+    let key = arguments.next().expect("Key was not found");
+    let value = arguments.next().unwrap();
 
-  let contents = format!("{}\t{}\n",key, value);
-  fs::write("kv.db", contents);
+    // println!("The Key is '{}' and the value is {}", key, value);
+    // we need a place to save the data. For now we will use a file.
 
+    let contents = format!("{}\t{}\n", key, value);
+    fs::write("kv.db", contents).unwrap();
+
+    
 }
